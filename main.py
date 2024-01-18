94467650c476f0a3dff10f8bbcfb62430106b745
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-01-18 14:31:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ kullanılan_hak=0
 while (kullanılan_hak<hak):
     tahmin=input("Bir sayi giriniz: ")
     if(tahmin!=sayi):
-        print("yanlış sayi girdiniz tekrar deneyin")
+        print("Yanlış sayi girdiniz tekrar deneyin")
         kullanılan_hak+=1
     else:
         print("doğru sayıyı bildiniz")    
